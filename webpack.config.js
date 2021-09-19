f60635e6ee4a404b1be7afcac4d6d63b3890fed9
Guillaume Gueyraud <g.gueyraud@greenassembly.fr>
2021-09-19 10:56:26 +0200

    Update project configuration

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = {
+    entry: {
+        global: './assets/js/global.js',
+        contact: './assets/js/contact.js',
+        project: './assets/js/project.js'
+    },
+    watch: process.env.NODE_ENV === 'development',
+    watchOptions: {
+        ignored: /node_modules/
+    },
+    target: "browserslist",
+    output: {
+        path: path.resolve(__dirname, 'dist')
+    },
+    experiments: {
+        syncWebAssembly: true,
+        asset: true
+    }
+}
