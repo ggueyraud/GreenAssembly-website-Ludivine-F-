f9dcc87399b609eb0366d9bc3fc3405c92f365c2
Guillaume Gueyraud <g.gueyraud@greenassembly.fr>
2021-09-21 00:08:12 +0200

    Implementing portfolio filters

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -9,11 +9,61 @@ const on_mount = () => {
                 if (e.target != active_btn_filter) {
                     active_btn_filter.classList.remove('active');
                     btn.classList.add('active');
-                }
 
-                console.log(e.target.dataset.id)
+                    sort(e.target.dataset.id)
+                }
             })
         })
+}
+
+let all_projects = [...document.querySelectorAll('.project')];
+
+const sort = id => {
+    const container = document.querySelector('#aaa');
+    container.animate([
+        { opacity: 100 },
+        { opacity: 0 }
+    ], { duration: 250 });
+
+    setTimeout(() => {
+        let projects = all_projects; //.map(project => project.outerHTML);
+    
+        if (id) {
+            projects = projects.filter(project => {
+                let categories = project.dataset.categories.split(';');
+    
+                return categories.includes(id)
+            });
+        }
+    
+        projects = projects.map(project => project.outerHTML);
+    
+        let grid = '';
+    
+        projects.forEach((project, index) => {
+            switch (index) {
+                case 0:
+                    grid += `<div class="col_span_2 row_span_2">${project}</div>`;
+                break;
+                case 1:
+                case 2:
+                    grid += `<div>${project}</div>`;
+                break;
+                case 3:
+                    grid += `<div class="col_span_2>${project}</div>`;
+                break;
+                default:
+                    grid += `<div>${project}</div>`;
+                break;
+            }
+        })
+    
+        container.innerHTML = grid
+        container.animate([
+            { opacity: 0 },
+            { opacity: 100 }
+        ], { duration: 250 });
+    }, 250)
 }
 
 const on_destroy = () => {
