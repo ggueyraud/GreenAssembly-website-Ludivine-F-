b874806ee6e2fd2b2d28cc45680d1301f3eaea90
Guillaume Gueyraud <g.gueyraud@greenassembly.fr>
2021-09-21 00:08:32 +0200

    Only returns categories which have projects

--- src/services/projects/categories.rs
+++ src/services/projects/categories.rs
@@ -10,7 +10,11 @@ pub async fn exists(pool: &PgPool, id: i16) -> bool {
 pub async fn get_all(pool: &PgPool) -> Vec<super::Category> {
     sqlx::query_as!(
         super::Category,
-        r#"SELECT id, name FROM project_categories ORDER BY "order""#
+        r#"SELECT
+            pc.id, pc.name
+        FROM project_categories pc
+        JOIN projects_categories pcs ON pcs.category_id = pc.id
+        ORDER BY "order""#
     )
     .fetch_all(pool)
     .await
