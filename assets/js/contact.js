fe5fc7f9af877b119d5627c1d5a02e3958a7ba6c
Guillaume Gueyraud <g.gueyraud@greenassembly.fr>
2021-09-21 22:46:42 +0200

    Contact page integration

---
+++ assets/js/contact.js
@@ -0,0 +1,32 @@
+const on_mount = () => {
+    document
+        .querySelectorAll('.input')
+        .forEach(input_container => {
+            const input = input_container.querySelector('input, textarea');
+
+            console.log(input instanceof HTMLTextAreaElement)
+
+            // Set autoheight
+            if (input instanceof HTMLTextAreaElement) {
+                input
+                    .addEventListener('input', e => {
+                        e.target.style.height = "5px";
+                        e.target.style.height = (e.target.scrollHeight)+"px";
+                    })
+            }
+
+            input
+                .addEventListener('blur', e => {
+                    console.log('ok')
+                    if (e.target.value.length > 0) {
+                        input_container.classList.add('valid')
+                    }
+                })
+        })
+}
+
+const on_destroy = () => {
+    window.removeEventListener('onMount', on_mount)
+}
+
+window.addEventListener('onMount', on_mount)
