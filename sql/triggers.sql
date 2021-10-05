ac493c0b7f16a760a1f819e40ce71b767b23cc7b
Guillaume Gueyraud <g.gueyraud@greenassembly.fr>
2021-10-06 00:23:11 +0200

    Add trigger for project categories ordering

--- sql/triggers.sql
+++ sql/triggers.sql
@@ -19,3 +19,60 @@ BEGIN
 END;
 $$ LANGUAGE PLPGSQL;
 CREATE trigger_insert_user BEFORE INSERT ON user ROW EXECUTE PROCEDURE insert_user();
+
+CREATE OR REPLACE FUNCTION delete_project_category() RETURNS TRIGGER AS $$
+DECLARE i SMALLINT;
+DECLARE row RECORD;
+BEGIN
+    i := 1;
+
+    FOR row IN SELECT id, "order" FROM project_categories
+        LOOP
+            RAISE LOG 'Attempt to update % from % to "order" value %', row.id, row.order, i;
+
+            UPDATE project_categories SET "order" = i WHERE id = row.id;
+            i := i + 1;
+        END LOOP;
+
+    RETURN OLD;
+END;
+$$ LANGUAGE PLPGSQL;
+DROP TRIGGER IF EXISTS trigger_delete_project_category ON project_categories;
+CREATE TRIGGER trigger_delete_project_category AFTER DELETE ON project_categories FOR EACH ROW EXECUTE PROCEDURE delete_project_category();
+
+CREATE OR REPLACE FUNCTION update_project_category() RETURNS TRIGGER AS $$
+DECLARE
+    row RECORD;
+    rows_ids SMALLINT[];
+    row_id SMALLINT;
+    space SMALLINT;
+BEGIN
+    IF NEW.order IS NOT NULL THEN
+        space := 1;
+
+        IF NEW.order > OLD.order THEN
+            SELECT ARRAY(SELECT id FROM project_categories WHERE "order" <= NEW.order AND id != NEW.id ORDER BY "order" DESC) INTO rows_ids;
+
+            FOREACH row_id IN ARRAY rows_ids
+            LOOP
+                UPDATE project_categories SET "order" = NEW.order - space WHERE id = row_id;
+
+                space := space + 1;
+            END LOOP;
+        ELSE
+            SELECT ARRAY(SELECT id FROM project_categories WHERE "order" >= NEW.order AND id != NEW.id ORDER BY "order" ASC) INTO rows_ids;
+
+            FOREACH row_id IN ARRAY rows_ids
+            LOOP
+                UPDATE project_categories SET "order" = NEW.order + space WHERE id = row_id;
+
+                space := space + 1;
+            END LOOP;
+        END IF;
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE PLPGSQL;
+DROP TRIGGER IF EXISTS trigger_update_project_category ON project_categories;
+CREATE TRIGGER trigger_update_project_category BEFORE UPDATE ON project_categories FOR EACH ROW WHEN (pg_trigger_depth() < 1) EXECUTE PROCEDURE update_project_category();
